a71aa81adfe9b64a926d21a93577111d1733e255
genivaldo32 <genivaldocicero32@gmail.com>
2022-08-29 12:48:08 -0300

    Adicionando um sidebar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+
 export default function NavBar() {
-   return (
+  const [showSideBar, setShowSidebar] = useState(false)
+
+  return (
     <div className="navbar container">
       <a href="#!" className="logo">F<span>oo</span>diesHub</a>
       <div className="nav-links">
         <a href="#!">Home</a>
         <a href="#!">Recipes</a>
         <a href="#!">Settings</a>
+      </div>
+      <div onClick={() => setShowSidebar(!showSideBar)} className={showSideBar ? "sidebar-btn active" : 'sidebar-btn'}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </div>
     )
